e1202292b8c5bd679d1ebd9477dfe69a8340947a
Sydney Sharp <sydney.marie.sharp@gmail.com>
2024-09-07 11:41:34 -0500

    updating css for edit and delete on my trips page

--- src/Pages/MyTrips/MyTrips.jsx
+++ src/Pages/MyTrips/MyTrips.jsx
@@ -60,13 +60,13 @@ const MyTrips = () => {
                                 <ul className="trip-details">
                                     <li>Start Date: {formatDate(trip.trip.start_date)}</li>
                                     <li>End Date: {formatDate(trip.trip.end_date)}</li>
+                                    </ul>
                                     {parseInt(trip.trip.user) === parseInt(userId) && (
-                                        <div className="trip-actions">
+                                        <div className="my-trip-button-section">
                                             <button onClick={() => handleEditClick(trip.trip.id)} className='edit-button>'>Edit</button>
                                             <button onClick={() => handleDeleteClick(trip.trip.id)} className="delete-button">Delete</button>
                                         </div>
                                     )}
-                                </ul>
                             </div>
                         </li>
                     ))}
